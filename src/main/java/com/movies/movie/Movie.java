82dcfab229f6e4c7ab52ab2a8b622b19527dff27
Deeraj Reddy Aalla <deerajgnt@gmail.com>
2019-09-19 21:12:46 -0500

    merge conflicts

--- src/main/java/com/movies/movie/Movie.java
+++ src/main/java/com/movies/movie/Movie.java
@@ -69,11 +69,11 @@ public class Movie {
     }
 
 
-    private void validateReleaseDate2(LocalDate modifiedReleaseDate){
+    private void validateReleaseDate2111(LocalDate modifiedReleaseDate,int wee){
         LocalDate currentDate = LocalDate.now();
     }
 
-    private void validateReleaseDat3(LocalDate modifiedReleaseDate){
+    private void validateReleaseDat3(LocalDate modifiedReleaseDate,int errr){
         LocalDate currentDate = LocalDate.now();
     }
     @JsonIgnore
